d369a0d640c0ac58408b600433d50794015dc943
Adrinems <adrianasorianom@gmail.com>
2014-11-03 08:58:42 -0600

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -6,7 +6,7 @@ flake8 --max-complexity=3 --exclude=*.txt,*.md --max-line-length=200 .
 
 lettuce AcceptanceTest
 cd UnitTest
-python TestCase.py -v
-coverage run --branch TestCase.py
+python TestCalculator.py -v
+coverage run --branch TestCalculator.py
 coverage report -m
 coverage html --title="Coverage about Figures"
